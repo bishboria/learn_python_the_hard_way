1450c6402d9c1abffeec92f7002b2b33f35537bb
Stuart Gale <bishboria@gmail.com>
2011-08-29 18:03:00 +0100

    Extra Credit for ex5

--- ex5.py
+++ ex5.py
@@ -14,3 +14,13 @@ print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
 
 print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
+
+# Extra Credit
+print "This uses %%r: %r." % (age * height * weight)
+# %r formats using repr() which returns a string that contains the printable
+# version of an object. Handy for classes later
+
+inches_centimetres_ratio = 2.54
+pounds_kilos_ratio = 2.2
+print "%s's height in centimetres is %.2f" % (name, height * inches_centimetres_ratio)
+print "%s's weight in kilos is %.2f" % (name, weight / pounds_kilos_ratio)

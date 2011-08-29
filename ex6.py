6feb254e4e6e97f62f816d30a0e650c26513ee87
Stuart Gale <bishboria@gmail.com>
2011-08-29 18:13:54 +0100

    Extra Credit for ex6

--- ex6.py
+++ ex6.py
@@ -1,13 +1,13 @@
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
-y = "Those who know %s and those who %s." % (binary, do_not)
+y = "Those who know %s and those who %s." % (binary, do_not) # 2 Strings in a string
 
 print x
 print y
 
-print "I said: %r." % x
-print "I also said: '%s'." % y
+print "I said: %r." % x # String in a string
+print "I also said: '%s'." % y # String in a string
 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
@@ -17,4 +17,7 @@ print joke_evaluation % hilarious
 w = "This is the left side of..."
 e = "a string with a right side."
 
-print w + e
+print w + e # + concatenates strings
+
+# There are only 4 strings in strings as x takes an integer
+# and "hilarious" is a boolean

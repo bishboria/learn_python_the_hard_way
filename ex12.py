70cc411a4002c4a3cdd38f4f9d9f317faecd9b21
Stuart Gale <bishboria@gmail.com>
2011-08-30 13:55:03 +0100

    Extra Credit for ex12

--- ex12.py
+++ ex12.py
@@ -3,3 +3,13 @@ height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weigh? ")
 
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+# Extra Credit: both open & file are used in file I/O. open is the preferred
+# way to do this though.
+
+# Import and use os if you want to make your programs compatible with *nix and
+# windows. But then you should limit yourself to only the functions available
+# to all systems.
+
+# sys gives you access to some of the functions available to the python
+# interpreter, like argv, path, stdout, stdin, float_info, maxint, etc

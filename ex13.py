2403d7cb18a2543760f2fd56e52d34208c089174
Stuart Gale <bishboria@gmail.com>
2011-08-30 14:22:53 +0100

    Extra Credit for ex13

--- ex13.py
+++ ex13.py
@@ -1,8 +1,17 @@
 from sys import argv
 
+print argv
 script, first, second, third = argv
 
 print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is", second
 print "Your third variable is", third
+
+# Extra Credit: passing the scrip less parameters gives a ValueError.
+# the LHS on line 3 is expecting 4 things, but argv doesn't have 4.
+
+# Giving more arguments also gives a ValueError. That's just daft...
+
+fourth_input = raw_input("Tell me something else: ")
+print argv + [fourth_input]

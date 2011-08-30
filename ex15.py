8f818e589b2005aac931ae9c9a90676e1369a95e
Stuart Gale <bishboria@gmail.com>
2011-08-30 16:38:38 +0100

    Extra Credit 6 for ex15

--- ex15.py
+++ ex15.py
@@ -1,12 +1,14 @@
 from sys import argv
 
-script, filename = argv
+script = argv
 
+filename = raw_input("type in the name of your file: ")
 txt = open(filename)
 
 print "Here's your file %r:" % filename
 print txt.read()
 
-txt_again = open(filename)
+file_again = raw_input("Type in the name of your file AGAIN: ")
+txt_again = open(file_again)
 
 print txt_again.read()

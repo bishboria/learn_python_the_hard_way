46d4698ca37aad61bbbf660e31729eb58f4aaaba
Stuart Gale <bishboria@gmail.com>
2011-08-30 16:35:55 +0100

    Extra Credit 4 for ex15

--- ex15.py
+++ ex15.py
@@ -7,9 +7,6 @@ txt = open(filename)
 print "Here's your file %r:" % filename
 print txt.read()
 
-print "I'll also ask you to type the filename again:"
-file_again = raw_input("> ")
-
-txt_again = open(file_again)
+txt_again = open(filename)
 
 print txt_again.read()

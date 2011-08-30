e7e042b8068cb6e4f4657b172d4b03c9acffb5c9
Stuart Gale <bishboria@gmail.com>
2011-08-30 16:49:48 +0100

    Extra Credit 8 for ex15

--- ex15.py
+++ ex15.py
@@ -7,8 +7,10 @@ txt = open(filename)
 bytes_to_read = 15
 print "Here's the first %d characters of the file %r:" % (bytes_to_read, filename)
 print txt.read(15)
+txt.close()
 
 txt_again = open(filename)
 
 print "Here's the first line of the file %r:" % filename
 print txt_again.readline()
+txt_again.close()

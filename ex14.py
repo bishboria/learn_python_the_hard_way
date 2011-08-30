35c90875cd06ebf48584ff602f206766e06cbcca
Stuart Gale <bishboria@gmail.com>
2011-08-30 14:36:25 +0100

    Extra Credit for ex14

--- ex14.py
+++ ex14.py
@@ -1,10 +1,11 @@
 from sys import argv
 
-script, user_name = argv
-prompt = '> '
+script, user_name, age = argv
+prompt = '### '
 
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
+print "First of all %r is quite old! LOLOAP" % age
 print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
 

3e4e3bb970460b277cce07dfb3eeef47ddb14b77
Stuart Gale <bishboria@gmail.com>
2011-08-31 11:25:40 +0100

    Extra Credit 3 for ex17

--- ex17.py
+++ ex17.py
@@ -1,15 +1,6 @@
 from sys import argv
-from os.path import exists
 
-script, from_file, to_file = argv
-
-# we could do these two on one line too, how?
-input = open(from_file)
-indata = input.read()
-output = open(to_file, 'w')
-output.write(indata)
-output.close()
-input.close()
+open(argv[2], 'w').write( open(argv[1]).read() )
 
 # if you just use import ex17mod then you call like this: ex17mod.testmod()
 from ex17mod import testmod

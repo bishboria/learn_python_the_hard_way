2e809e1ff96799f80ac5bdf885f2b9c23631c751
Stuart Gale <bishboria@gmail.com>
2011-09-01 14:37:56 +0100

    Extra Credit for ex25

--- ex25.py
+++ ex25.py
@@ -33,3 +33,21 @@ def print_first_and_last_sorted(sentence):
     words = sort_sentence(sentence)
     print_first_word(words)
     print_last_word(words)
+
+# Extra Credit
+# >>> ex25.print_first_word(sorted_words)                           # printing out the first word in the sorted_words list
+# All                                                               # which pops the word off the list
+# >>> ex25.print_last_word(sorted_words)                            # same again but with the last word
+# who
+# >>> sorted_words                                                  # shows the state of sorted_words after all the popping
+# ['come', 'good', 'things', 'those', 'to', 'wait.']
+# >>> sorted_words = ex25.sort_sentence(sentence)                   # sorts the sentence
+# >>> sorted_words                                                  # and shows the list
+# ['All', 'come', 'good', 'things', 'those', 'to', 'wait.', 'who']
+# >>> ex25.print_first_and_last(sentence)                           # print the first and last word in the sentence
+# All
+# wait.
+# >>> ex25.print_first_and_last_sorted(sentence)                    # sorts the sentence before printing the first and last sentence
+# All
+# who
+# >>> ^D                                                            # exit the repl

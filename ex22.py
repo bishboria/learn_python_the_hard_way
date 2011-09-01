ce6b1c1b8809601795a1893e94edc7a39fad6713
Stuart Gale <bishboria@gmail.com>
2011-09-01 12:40:38 +0100

    Adding ex22

---
+++ ex22.py
@@ -0,0 +1,28 @@
+#
+# print
+# *
+# /
+# -
+# +
+# =
+# <
+# >
+# <=
+# >=
+# , in string printing
+# % in string formatting
+# "/'
+# """
+# \n \t \\
+# raw_input
+# from
+# import
+# sys
+#   argv
+# os.path
+#   exists
+# a, b, c, d = e
+# open(name, arg) arg r/w/a(+) { b U }
+#   read, readline, write, close, seek, truncate
+# def method_name(arg, *list_of_args):
+# return

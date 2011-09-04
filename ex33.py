0bd77dac16eb0fdb3183a31051458f81852b327c
Stuart Gale <bishboria@gmail.com>
2011-09-04 14:38:18 +0100

    Extra Credit 5 for ex33

--- ex33.py
+++ ex33.py
@@ -28,3 +28,18 @@ def print_append_loop(count, increment):
         print "At the bottom i is %d" % i
 
 print_append_loop(5, 2)
+
+def print_append_loop_2(count, increment):
+    numbers = []
+    for i in range(0, count, increment):
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i += 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+print_append_loop_2(8, 3)
+# You don't need to add the manual increment in the code
+# And it produces the same list in numbers as it's running...
+# Even though i through the loop is different

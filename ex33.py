88c46551eb47bc091c89139a44ac20cdcd642129
Stuart Gale <bishboria@gmail.com>
2011-09-04 14:29:27 +0100

    Extra Credit 1 & 2 for ex33

--- ex33.py
+++ ex33.py
@@ -14,3 +14,17 @@ print "The numbers: "
 
 for num in numbers:
     print num
+
+# Extra Credit
+numbers = []
+def print_append_loop(count):
+    i = 0
+    while i < count:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i += 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+print_append_loop(3)
